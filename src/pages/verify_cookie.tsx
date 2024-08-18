8fb4d096021ded57263fd846d2456257651b061f
neko_ninja <timothy_shee@hotmail.com>
2024-08-18 00:53:09 -0400

    Create verify_cookie.tsx

---
+++ src/pages/verify_cookie.tsx
@@ -0,0 +1,43 @@
+// pages/dashboard.tsx
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+const Dashboard = () => {
+  const [user, setUser] = useState<{ username: string } | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      // const response = await fetch('/api/oauth_login');
+      const response = await fetch('http://localhost:8123/api/verify_cookie');
+      
+      if (response.ok) {
+        const resp = await response.json();
+        setUser(resp.data);
+      } else {
+        router.push('/');
+        setError('Not authenticated');
+      }
+    };
+
+    fetchUser();
+  }, [router]);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!user) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome, {user.username}!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
